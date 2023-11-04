baedde27ace6a7672e4cf789d0d2d4491bab26d3
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-03 21:44:40 -0400

    Initial Commit

---
+++ Veganize/VeganizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeganizeApp.swift
+//  Veganize
+//
+//  Created by Danielle Lewis on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct VeganizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
