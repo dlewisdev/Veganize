2e336c82cd942a0e2cf742f80f48479c83d7dc5a
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-06 22:22:07 -0500

    Adds search bar and navigation title to Home View.

--- Veganize/Views/Home/HomeView.swift
+++ Veganize/Views/Home/HomeView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var prompt: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                
+            }
+            .navigationTitle("Veganize")
+            .searchable(text: $prompt)
+        }
+        
     }
+
 }
 
 #Preview {
