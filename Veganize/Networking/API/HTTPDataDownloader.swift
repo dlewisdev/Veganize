edac64e6bc79a487e60541d3ddb367638ded6687
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-06 22:21:23 -0500

    Adds generic protocol for fetching data from endpoints.

--- Veganize/Networking/API/HTTPDataDownloader.swift
+++ Veganize/Networking/API/HTTPDataDownloader.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+protocol HTTPDataDownloader {
+    func fetchData<T: Decodable>(as type: T.Type, endpoint: String) async throws -> T
+}
+
+extension HTTPDataDownloader {
+     func fetchData<T: Decodable>(as type: T.Type, endpoint: String) async throws -> T {
+        
+        guard let url = URL(string: endpoint) else {
+            throw RecipeAPIError.requestFailed(description: "Invalid URL")
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw RecipeAPIError.requestFailed(description: "Request Failed")
+        }
+        
+        guard httpResponse.statusCode == 200  else {
+            throw RecipeAPIError.invalidStatusCode(statusCode: httpResponse.statusCode)
+        }
+        
+        do {
+            return try JSONDecoder().decode(type, from: data)
+        } catch {
+            print("DEBUG: Error \(error.localizedDescription)")
+            throw error as? RecipeAPIError ?? .unknownError(error: error)
+        }
+    }
+}
