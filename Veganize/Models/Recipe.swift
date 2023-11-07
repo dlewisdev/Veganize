565f776e119928df2230fe8db3944b8b4563f037
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-06 22:22:24 -0500

    Adds struct for recipes and conforms to Codable.

--- Veganize/Models/Recipe.swift
+++ Veganize/Models/Recipe.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct Recipe: Codable {
+    let id: Int
+    let title: String
+    let image: String
+}
+
+struct RecipeDetails: Codable {
+    let recipe: Recipe
+    let title: String
+    let instructions: [String]
+    let ingredients: [String]
+}
+
