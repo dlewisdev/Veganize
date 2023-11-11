aa74042d4d3ac578073262ddad9f4bdad81f6ef6
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-10 23:24:12 -0500

    Adds functionality for fetching recipe details.

--- Veganize/ViewModels/RecipeDetailsViewModel.swift
+++ Veganize/ViewModels/RecipeDetailsViewModel.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+class RecipeDetailsViewModel: ObservableObject {
+    private let service: RecipeServiceProtocol
+    private let recipeId: Int
+    
+    @Published var recipeDetails: RecipeDetails?
+    
+    init(recipeId: Int, service: RecipeServiceProtocol) {
+        self.service = service
+        self.recipeId = recipeId
+    }
+    
+    @MainActor
+    func fetchRecipeDetails() async {
+        do {
+            self.recipeDetails = try await service.fetchRecipeDetails(id: recipeId)
+        } catch {
+            print("DEBUG: Error \(error.localizedDescription)")
+        }
+    }
+}
