37d674a26c83a15ebdba57c5e8d14f6999d84280
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-10 23:21:47 -0500

    Adds testing for decoding recipes.

--- VeganizeTests/VeganizeTests.swift
+++ VeganizeTests/VeganizeTests.swift
@@ -9,5 +9,12 @@ import XCTest
 @testable import Veganize
 
 final class VeganizeTests: XCTestCase {
-
+    
+    func test_DecodeRecipesIntoArray() throws {
+        do {
+            let recipeResponse = try JSONDecoder().decode(RecipeResponse.self, from: mockRecipeData)
+            let recipes = recipeResponse.results
+            XCTAssertTrue(recipes.count > 0)
+        }
+    }
 }
