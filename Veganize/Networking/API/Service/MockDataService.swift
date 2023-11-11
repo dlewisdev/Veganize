20bf895b99661f29a0ad2766fd35b92f92c776ef
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-10 23:24:56 -0500

    Adds mock data service to save API calls.

--- Veganize/Networking/API/Service/MockDataService.swift
+++ Veganize/Networking/API/Service/MockDataService.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+class MockDataService: RecipeServiceProtocol {
+    
+    var mockData: Data?
+    var mockError: RecipeAPIError?
+    
+    func fetchRecipes(query: String) async throws -> RecipeResponse {
+        if let mockError { throw mockError }
+        
+        do {
+            let result = try JSONDecoder().decode(RecipeResponse.self, from: mockData ?? mockRecipeData)
+            return result
+        }
+    }
+    
+    func fetchRecipeDetails(id: Int) async throws -> RecipeDetails? {
+        let details = RecipeDetails(id: "12345", title: "", instructions: [""], ingredients: [""])
+        return details
+    }
+    
+    
+}
