1fafcffbe4ac960c8f90c540e32a235f43faee1e
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-10 23:23:34 -0500

    Adds recipes to home page and fixes search field.

--- Veganize/Views/Home/HomeView.swift
+++ Veganize/Views/Home/HomeView.swift
@@ -8,21 +8,44 @@
 import SwiftUI
 
 struct HomeView: View {
-    @State private var prompt: String = ""
+    private let service: RecipeServiceProtocol
+    @StateObject var viewModel: RecipeViewModel
+    @State var didSubmitSearch = false
+    @Environment(\.dismissSearch) var dismissSearch
+    
+    init(service: RecipeServiceProtocol) {
+        self.service = service
+        self._viewModel = StateObject(wrappedValue: RecipeViewModel(service: service))
+    }
     
     var body: some View {
         NavigationStack {
-            VStack {
-                
+            ScrollView {
+                LazyVStack {
+                    ForEach(viewModel.recipes) { recipe in
+                        NavigationLink(value: recipe) {
+                            RecipeCard(recipe: recipe)
+                                .padding()
+                        }
+                        .buttonStyle(.plain)
+                    }
+                }
+                .navigationDestination(for: Recipe.self, destination: { recipe in
+                    RecipeDetailsView(recipe: recipe, service: service)
+                })
             }
             .navigationTitle("Veganize")
-            .searchable(text: $prompt)
+            .searchable(text: $viewModel.searchQuery)
+            .onSubmit(of: .search) {
+                Task { await viewModel.fetchRecipeSearchResults() }
+            }
+            
         }
         
     }
-
+    
 }
 
 #Preview {
-    HomeView()
+    HomeView(service: MockDataService())
 }
