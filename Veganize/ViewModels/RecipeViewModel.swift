9060a4b67e60cd981b8894075489ca2af2751cec
Danielle Lewis <msdaniellelewis@icloud.com>
2023-11-10 23:23:56 -0500

    Adds recipe view model to project.

--- Veganize/ViewModels/RecipeViewModel.swift
+++ Veganize/ViewModels/RecipeViewModel.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+class RecipeViewModel: ObservableObject {
+    
+    
+    @Published var recipes = [Recipe]()
+    @Published var errorMessage: String?
+    @Published var searchQuery: String = ""
+    
+    private let service: RecipeServiceProtocol
+    
+    init(service: RecipeServiceProtocol) {
+        self.service = service
+    }
+    
+    @MainActor
+    func fetchRecipeSearchResults() async {
+        do {
+            let recipeResponse = try await service.fetchRecipes(query: searchQuery)
+            self.recipes = recipeResponse.results
+            
+            
+            print("DEBUG: Did fetch recipes")
+        } catch {
+            if let error = error as? RecipeAPIError {
+                self.errorMessage = error.customDescription
+            } else {
+                self.errorMessage = error.localizedDescription
+            }
+        }
+    }
+    
+    
+}
